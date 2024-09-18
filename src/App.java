4bba7899347bb3cce9b14aa20426f7408d6b8616
Your Name <you@example.com>
2024-09-18 15:28:44 +0300

    method 2 to make random groups

--- src/App.java
+++ src/App.java
@@ -13,5 +13,18 @@ public class App {
         students.add("Naba");
         System.out.println(students);
     }
+    public void randomGroups(ArrayList<String>  students){
+        ArrayList<String>  group1 = new ArrayList<>();
+        ArrayList<String>  group2 = new ArrayList<>();
+        
+            group1.add(students.get(0));
+            group1.add(students.get(3));
+            group2.add(students.get(2));
+            group2.add(students.get(1));
+        
+        System.out.println("group1"+ group1);
+        System.out.println("group2 "+ group2);
+        
+    }
     
 }
